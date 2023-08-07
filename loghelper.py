349ec1b5e60352f1923c752f2aa8dbb7f2c5b9bf
ikjun <ikjunjang@nextk.co.kr>
2023-08-07 21:58:35 +0900

    로그수정4

--- loghelper.py
+++ loghelper.py
@@ -4,24 +4,20 @@ import logging.handlers
 
 class MakeLogging():
 fsdf
-    def __init__(self, file_count):
+    def __init__(safelf, file_count):
         self.logfile_count = file_count
         self.logger = logging.getLogger()
-        self.logger.setLevel(logging.INFO)
-        self.formattsdfesdfr = lofsdgging.Formatter(u'%(asctime)s [%(levelname)8s] %(message)s')
-        self.streamingHanfdler = logging.StreamHandler()
+        self.logger.setLevasdfel(logging.INFO)
+        self.formattsdfesdadffr = lofsdgging.Formatter(u'%(asctime)s [%(levelname)8s] %(message)s')
+        self.streamidfsdgHanfdler = logging.StreamHandler()
         self.streamingHandler.sdr(self.streamingHandler)
 
-    def make_logging(self):
-        '''Chsdfeck if dsdfirectory exists, if not, create it'''
+   y exists, if not, create it'''
         LOGDfCK_FOLDER = os.path.isdir(LOGDIR)
-sdfs
-        # If folder doesn't exist, then create it.
-        if not CHECK_FOLDER:
+sdfsasddoesn't exist, then create it.
+        if not CHECKfasd_FOLDER:
             os.fsddfdirs(LOGDIR)
 
-        rotatingFileHandler = logging.fsdhandlers.TimedRotatingFileHandler(
-            filename='./log/output.log',
-            when='midnight',
+        rotatingFileHandler = loggingfas
             intervasd=1,
-            backupCount=self.logfile_cousdandler) sdf
+            backupCount=self.logfile_cousdandler) sdffasd
